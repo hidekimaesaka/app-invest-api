364345c9f07ae5a296ecd5d64ff2d6e727dcaafe
Hideki Maesaka <lucasmsk11@gmail.com>
2022-01-19 14:14:43 -0300

    ajuste no metodo listen

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const User = require("./models/User")
 
 //Rota pública
 app.get("/", (req, res) => {
-    res.status(200).json({msg: "Bem vindo a API"})
+    res.status(200).json({ msg: "Bem vindo a API" })
 })
 
 //Rota privada
@@ -31,11 +31,11 @@ app.get("/user/:id", checkToken, async (req, res) => {
     //Verifica se o usuário existe
     const user = await User.findById(id, "-password")
 
-    if(!user) {
-        return res.status(404).json({msg: "Usuário não encontrado!"})
+    if (!user) {
+        return res.status(404).json({ msg: "Usuário não encontrado!" })
     }
 
-    res.status(200).json({user})
+    res.status(200).json({ user })
 })
 
 
@@ -45,19 +45,19 @@ function checkToken(req, res, next) {
     const authHeader = req.headers["authorization"]
     const token = authHeader && authHeader.split(" ")[1]
 
-    if(!token) {
-        return res.status(401).json({msg: "Acesso negado!"})
+    if (!token) {
+        return res.status(401).json({ msg: "Acesso negado!" })
     }
 
-    try{
+    try {
 
         const secret = process.env.SECRET
         jwt.verify(token, secret)
 
         next()
 
-    }catch(error){
-        res.status(400).json({msg: "Token inválido!"})
+    } catch (error) {
+        res.status(400).json({ msg: "Token inválido!" })
     }
 
 }
@@ -65,38 +65,38 @@ function checkToken(req, res, next) {
 //Rota de Login de usuário
 app.post("/auth/login", async (req, res) => {
 
-    const {email, password, _id} = req.body
+    const { email, password, _id } = req.body
 
     /*Verifica se o usuário existe E instancia um objeto de usuário referente
     ao email inserido */
-    const user = await User.findOne({email: email})
+    const user = await User.findOne({ email: email })
 
-    if(!user) {
-        return res.status(422).json({ msg: "Usuário não encontrado"})
+    if (!user) {
+        return res.status(422).json({ msg: "Usuário não encontrado" })
     }
 
     //Verifica se o usuário acertou a senha
 
     const checkPassword = await bcrypt.compare(password, user.password)
 
-    if(!checkPassword) {
+    if (!checkPassword) {
         return res.status(404).json({ msg: "Senha inválida!" })
     }
 
-    try{
+    try {
 
         const secret = process.env.SECRET
         const token = jwt.sign(
             {
-            id: user._id,
-            }, 
+                id: user._id,
+            },
             secret,
         )
-        
-        res.status(200).json({msg: "Autenticação realizada com sucesso", token, id: user._id})
 
-    } catch (error){
-        res.status(500).json({msg: error})
+        res.status(200).json({ msg: "Autenticação realizada com sucesso", token, id: user._id })
+
+    } catch (error) {
+        res.status(500).json({ msg: error })
     }
 
 })
@@ -107,35 +107,35 @@ app.post("/auth/login", async (req, res) => {
 //Rota de Registro de usuário
 app.post("/auth/register", async (req, res) => {
 
-    const {name, email, password} = req.body
+    const { name, email, password } = req.body
 
-   //Verfica se o usuário ja existe
-   const userExists = await User.findOne({email: email})
+    //Verfica se o usuário ja existe
+    const userExists = await User.findOne({ email: email })
 
-   if(userExists) {
-       return res.status(442).json({msg: "e-Mail já cadastrado!"})
-   }
+    if (userExists) {
+        return res.status(442).json({ msg: "e-Mail já cadastrado!" })
+    }
 
-   //Criar a senha
-   const salt = await bcrypt.genSalt(12)
-   const passwordHash = await bcrypt.hash(password, salt)
+    //Criar a senha
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
 
-   //Criar usuário
-   const user = new User({
-       name,
-       email,
-       password: passwordHash,
-   })
+    //Criar usuário
+    const user = new User({
+        name,
+        email,
+        password: passwordHash,
+    })
 
-   try{
+    try {
 
-    await user.save()
-    
-    res.status(201).json({msg: "Usuário criado com sucesso!"})
+        await user.save()
 
-   } catch(error) {
-       res.status(500).json({msg: error})
-   }
+        res.status(201).json({ msg: "Usuário criado com sucesso!" })
+
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
 
 })
 
@@ -145,9 +145,9 @@ const dbPassword = process.env.DB_PASS
 
 // Tenta se conectar ao banco. Se conseguir abre a porta, se não printa o erro
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.kthlt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
-.then(() => {
-    app.listen(process.env.PORT || 3001)
-    console.log("Conectou ao banco com sucesso!")
-})
-.catch((error) => console.log(error))
+    .then(() => {
+        app.listen(3001);
+        console.log("Conectou ao banco com sucesso!")
+    })
+    .catch((error) => console.log(error))
 
